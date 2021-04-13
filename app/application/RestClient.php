a026946c3f8f45ce1a5d2334a4baf03b1b9ffd36
pasa <milanpasic92@gmail.com>
2021-04-13 15:32:56 +0200

    restclient added

---
+++ app/application/RestClient.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LearningKey\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use Phalcon\Di;
+use Phapi\Application\ApiError;
+
+class RestClient{
+
+    private Client $client;
+    private ?Di\DiInterface $di;
+
+    public function __construct($serviceName)
+    {
+        $this->di = DI::getDefault();
+        $this->client = new Client(['base_uri' => "http://$serviceName/"]);
+    }
+
+    public function get($route){
+        $client = new Client([]);
+
+        try {
+            $request = $client->request('GET', $route, [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->di->get('user')->token
+                ]
+            ]);
+            $data = $request->getBody()->getContents();
+            return json_decode($data);
+        }
+        catch (ClientException $e){
+            return new ApiError([$e->getMessage()], [
+                'status_code' => $e->getCode()
+            ]);
+        }
+    }
+}
