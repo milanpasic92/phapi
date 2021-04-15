ee7e3fa1c8b57178f9178cbbe8eba88bde92b1e1
pasa <milanpasic92@gmail.com>
2021-04-15 17:35:21 +0200

    headers fix

--- app/application/CorsMiddleware.php
+++ app/application/CorsMiddleware.php
@@ -2,7 +2,6 @@
 
 namespace Phapi\Application;
 
-use Phalcon\DI;
 use Phalcon\Events\Event;
 use Phalcon\Mvc\Micro;
 
@@ -18,7 +17,9 @@ class CorsMiddleware
 
         $application->di->get('rest')->response->setHeader('Access-Control-Allow-Origin', $origin)
             ->setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-            ->setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, *')
+            ->setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Origin, *')
             ->setHeader('Access-Control-Allow-Credentials', 'true');
+
+        $application->di->get('rest')->response->sendHeaders();
     }
 }
