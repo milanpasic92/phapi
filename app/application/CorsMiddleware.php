ab8c61b3087d9081c1b4fc1cc87858cec56551d6
milanpasic92 <milanpasic92@gmail.com>
2021-04-29 14:28:41 +0200

    cors fix

--- app/application/CorsMiddleware.php
+++ app/application/CorsMiddleware.php
@@ -9,15 +9,15 @@ class CorsMiddleware
 {
     public function beforeHandleRoute(Event $event, Micro $application)
     {
-        if ($application->di->get('rest')->request->getHeader('Access-Control-Allow-Origin')) {
-            $origin = $application->di->get('rest')->request->getHeader('Access-Control-Allow-Origin');
+        if ($application->di->get('rest')->request->getHeader('Origin')) {
+            $origin = $application->di->get('rest')->request->getHeader('Origin');
         } else {
             $origin = '*';
         }
 
         $application->di->get('rest')->response->setHeader('Access-Control-Allow-Origin', $origin)
             ->setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-            ->setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Origin, *')
+            ->setHeader('Access-Control-Allow-Headers', 'Content-Type, Origin, Access-Control-Allow-Origin, *')
             ->setHeader('Access-Control-Allow-Credentials', 'true');
 
         $application->di->get('rest')->response->sendHeaders();
