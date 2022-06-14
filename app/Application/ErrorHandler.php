14a6701568dff618925f1e082a57885b900898d1
milanpasic92 <milan.pasic@badin.rs>
2022-06-14 15:37:06 +0200

    errorHandler silent errors support

--- app/Application/ErrorHandler.php
+++ app/Application/ErrorHandler.php
@@ -7,6 +7,9 @@ use Phapi\Exceptions\BaseException;
 
 class ErrorHandler
 {
+    const SKIP = [
+        'bytes exceeds the limit of' // POST Content-Type warnings for posting files bigger than php_ini `upload_max_filesize`
+    ];
 
     public static function run()
     {
@@ -20,13 +23,24 @@ class ErrorHandler
 
             if($error !== NULL && $error['type'] <= 2) // catch only warnings or lower
             {
-                $errno = $error["type"];
-                $errfile = $error["file"];
-                $errline = $error["line"];
-                $errstr = $error["message"];
+                $report = true;
 
-                $exception = new BaseException($errstr, 0, $errno, $errfile, $errline);
-                $exception->handle();
+                foreach (self::SKIP as $message){
+                    if(strpos($error['message'], $message) !== false){
+                        $report = false;
+                        break;
+                    }
+                }
+
+                if($report) {
+                    $errno = $error["type"];
+                    $errfile = $error["file"];
+                    $errline = $error["line"];
+                    $errstr = $error["message"];
+
+                    $exception = new BaseException($errstr, 0, $errno, $errfile, $errline);
+                    $exception->handle();
+                }
             }
         });
     }
